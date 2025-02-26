e75d9daaa6cf0946953e6c6c2ee9f2918d24d638
ITSid30 <siddhesh.bajad@gmail.com>
2025-02-26 20:49:31 +0530

    [Backend]: Made API changes for Autoincrementing ID

--- src/main/java/com/application/expense/entities/Expense.java
+++ src/main/java/com/application/expense/entities/Expense.java
@@ -1,12 +1,15 @@
 package com.application.expense.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Expense {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String description;
 	private double amount;
@@ -48,9 +51,8 @@ public class Expense {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Expense(long id, String description, double amount, String type, long date) {
+	public Expense(String description, double amount, String type, long date) {
 		super();
-		this.id = id;
 		this.description = description;
 		this.amount = amount;
 		this.type = type;
